a43863153d7d8d24b6c6101225d1e3a179a4eafb
Laura Hart <onemansavedall@hotmail.com>
2024-03-19 11:51:19 -0500

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Laura Hart on 3/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
